30b9395712a58649607d45e10d9241ca2dc574f9
Jay Ni <signed121@gmail.com>
2021-04-17 20:10:22 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let main = document.getElementById('main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+let text = document.createTextNode('MOMO is the champion');
+newHeader.appendChild(text)
